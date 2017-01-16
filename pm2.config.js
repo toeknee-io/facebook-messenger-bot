33a8c8818f38f79709eabcb82018779056bfd69c
toeknee <toeknee@toeknee.io>
2017-01-16 00:43:51 -0500

    some conf changes

--- pm2.config.js
+++ pm2.config.js
@@ -1,10 +1,8 @@
-console.dir(process.env);
-
 module.exports = {
   apps: [
     {
       name: 'facebook-messenger-bot',
-      script: '/usr/local/nodejs/facebook-messenger-bot.js',
+      script: 'index.js',
       exec_mode: 'fork',
       instances: 1,
       env: {},
