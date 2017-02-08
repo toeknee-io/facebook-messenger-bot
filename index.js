72082d5880f3146ab12e417372c37c68e8f183a6
toeknee <toeknee@toeknee.io>
2017-02-08 00:10:31 -0500

    ccmmddd

--- index.js
+++ index.js
@@ -1,260 +1,109 @@
 const fs = require('fs');
 const _ = require('lodash');
-const path = require('path');
-const mmm = require('mmmagic');
 const request = require('request');
+const cmd = require('./lib/cmd.js');
 const emoji = require('node-emoji');
-const rp = require('request-promise');
 const config = require('./config.json');
 const utils = require('./lib/utils.js');
-const emojiRegex = require('emoji-regex');
-const moment = require('./lib/moment-extended.js');
-
-const magic = new mmm.Magic(mmm.MAGIC_MIME);
+const stickers = require('./data/facebook-stickers.json');
 
 const ENV = process.env.NODE_ENV;
 const DIR_ART = `${__dirname}/art`;
 const DIR_GIF = `${__dirname}/gif`;
+
 const credentials = utils.getCredentials();
-
-const addArtPending = [];
-
-const REPLY_BAD_CMD = [
+const badCmdRes = [
   'No', 'In you own ass', 'Eff that', '¯\u005C_(ツ)_/¯',
   { sticker: '1057971357612846' },
   { attachment: fs.createReadStream(`${DIR_ART}/1.jpg`, 'utf8') },
 ];
 
+let intervalId;
 let writeLock = false;
-let artFiles = fs.readdirSync(DIR_ART, 'utf8');
 
-require('facebook-chat-api')(credentials, (loginErr, chat) => {
-  if (loginErr) {
-    throw loginErr;
+utils.login(credentials).then((chat) => {
+  function resetRandom() {
+    clearInterval(intervalId);
+    intervalId = setInterval(() => doRandom(), 5000);
   }
+    
+  function doRandom() {
+    resetRandom();
 
-  utils.writeAppState(chat.getAppState());
-  chat.setOptions(config.chat.options);
+    const i = _.random(2);
 
-  function sendMsg(msg, toId) {
-    let recipient = toId;
-    if (ENV === 'development') {
-      recipient = config.facebook.userId.tony;
-      chat.sendMessage(`[DEBUG] ${JSON.stringify(msg)}`, recipient);
+    let msg;
+
+    if (i === 0) {
+      msg = utils.getRandomFromArray(badCmdRes);
+    } else if (i === 1) {
+      const stickersv = _.toArray(stickers);
+      const idx = _.random(stickersv.length - 1);
+      msg = { sticker: stickersv[idx].stickerID };
+    } else {
+      const artFiles = utils.getArtFiles();
+      console.dir(artFiles);
+      const idx = _.random(artFiles.length - 1);
+      msg = { attachment: fs.createReadStream(`${DIR_ART}/${utils.getArtFiles()[idx]}`, 'utf8') };
     }
-    chat.sendMessage(msg, recipient);
-  }
 
-  setTimeout(() => utils.checkPresence(chat), 60000);
+    chat.sendMsg(msg, '1184034474942360');
+  }
 
   const stopListening = chat.listen((listenErr, event) => {
     if (listenErr) {
       throw listenErr;
     }
 
-    if (utils.isBot(event) || utils.isCooldown(event)) {
+    const isMsg = event.type === 'message';
+
+    resetRandom();
+    utils.logEvent(event);
+
+    if (utils.isBot(event) || utils.isCooldown(event) || !isMsg) {
       utils.debug(`skipping event: ${JSON.stringify(event)}
         isBot ${utils.isBot(event)} isCooldown ${utils.isCooldown(event)}`);
       return;
     }
 
     utils.assignEventProps(event);
-    utils.logEvent(event);
 
-    const cmd = utils.getCmd(event);
-    const attachv = event.attachments;
     const toId = ENV !== 'development'
       ? event.threadID
       : config.facebook.userId.tony;
 
     if (event.senderName === 'jerry') {
-      const msg = _.isArray(attachv) && attachv.length
-        ? utils.getRandomFromArray(REPLY_BAD_CMD)
+      const msg = event.attachv.length
+        ? utils.getRandomFromArray(badCmdRes)
         : utils.getJerryReply();
-      chat.sendMessage(msg, toId);
-    } else if (event.body && event.body.length <= 2
-    && emojiRegex().test(event.body)) {
-      const msg = event.body === '🔥' ? '🔥' : emoji.random();
-      chat.sendMessage(msg, toId);
-    } else if (utils.inArtQueue(addArtPending, event)
-    && utils.canWrite(writeLock, attachv)) {
+      chat.sendMsg(msg, toId);
+    } else if (utils.isSaveArtEvent(writeLock, event)) {
       writeLock = true;
       fs.readdir(DIR_ART, 'utf8', (readErr, files) => {
-        request(attachv[0].previewUrl)
-          .pipe(fs.createWriteStream(`${DIR_ART}/${files.length}.jpg`, 'utf8'))
-          .on('close', () => {
-            writeLock = false;
-            addArtPending.splice(addArtPending.indexOf(event.senderID), 1);
-            chat.sendMessage(`Art has been added at index ${files.length}`, toId);
-            artFiles = fs.readdirSync(DIR_ART, 'utf8');
-          });
+        request(event.attachv[0].previewUrl)
+         .pipe(fs.createWriteStream(`${DIR_ART}/${files.length}.jpg`, 'utf8'))
+         .on('close', () => {
+           writeLock = false;
+           utils.rmArtQueue(event);
+           chat.sendMsg(`Art has been added at index ${files.length}`, toId);
+           utils.setArtFiles(fs.readdirSync(DIR_ART, 'utf8'));
+         });
       });
-    } else if (cmd) {
-      const subCmd = utils.getSubCmd(cmd, event);
-
-      if (cmd === 'art') {
-        if (subCmd) {
-          if (subCmd === 'add') {
-            addArtPending.push(event.senderID);
-          } else if (subCmd === 'gallery') {
-            const attachment = [];
-            artFiles.forEach((file) => {
-              const filePath = `${DIR_ART}/${file}`;
-              magic.detectFile(filePath, (err, result) => {
-                if (err) {
-                  console.error(err);
-                } else {
-                  if (result.match(/jpeg|png/)) {
-                    attachment.push(fs.createReadStream(filePath));
-                  } else {
-                    console.error(`invalid art type (${result}): ${filePath}`);
-                    artFiles.splice(artFiles.indexOf(file), 1);
-                    fs.unlink(filePath, (unlinkErr) => {
-                      if (unlinkErr) {
-                        console.error(`failed to deleted invalid art file: ${filePath}`);
-                      }
-                    });
-                  }
-                  if (attachment.length === artFiles.length) {
-                    chat.sendMessage({ attachment }, toId);
-                  }
-                }
-              });
-            });
-          } else if (subCmd === 'refresh') {
-            let msg = 'Art has been refreshed:\u000A\u000A';
-
-            artFiles = _.sortBy(fs.readdirSync(DIR_ART, 'utf8'), file => _.toNumber(file.replace('.jpg', '')));
-            artFiles.forEach(file => (msg += `${file.replace('.jpg', '')}\u000A`));
-
-            chat.sendMessage(msg, toId);
-          } else if (subCmd === 'list') {
-            artFiles.forEach(file =>
-              chat.sendMessage(
-                { body: `/art ${file.replace('.jpg', '')}`, attachment: fs.createReadStream(`${DIR_ART}/${file}`) },
-                event.senderID
-              ));
-          } else {
-            const fileName = path.extname(subCmd) === '.jpg' ? subCmd : `${subCmd}.jpg`;
-            const msg = artFiles.indexOf(fileName) > -1
-              ? { attachment: fs.createReadStream(`${DIR_ART}/${fileName}`) }
-              : utils.getRandomFromArray(REPLY_BAD_CMD);
-
-            chat.sendMessage(msg, toId);
-          }
-        } else {
-          const file = utils.getRandomFromArray(artFiles);
-
-          chat.sendMessage({
-            body: `/art ${file.replace('.jpg', '')}`,
-            attachment: fs.createReadStream(`${DIR_ART}/${file}`),
-          }, toId);
-        }
-      }
-      if (cmd === 'trump') {
-        if (subCmd && (_.lowerCase(subCmd) === 'tony' || _.lowerCase(subCmd) === 'trump')) {
-          chat.sendMessage(`${subCmd} is making bots great again`, toId);
-        } else {
-          const opts = {
-            uri: `${config.trump.api.uri}/${subCmd ? 'personalized' : 'random'}`,
-            qs: { q: subCmd },
-            json: true,
-          };
-          rp(opts).then((json) => {
-            const msg = { body: `"${json.message}"`, attachment: fs.createReadStream(`${DIR_ART}/18.jpg`) };
-            chat.sendMessage(msg, toId);
-          }).catch(err => console.error(`trump err: ${err}`));
-        }
-      }
-      if (cmd === 'kick') {
-        const kickId = config.facebook.userId[subCmd];
-        if (event.senderName === 'tony' && kickId) {
-          console.log(`kick: ${subCmd} (${kickId}) from ${event.threadID}`);
-          chat.removeUserFromGroup(kickId, event.threadID);
-        } else {
-          chat.sendMessage(utils.getRandomFromArray(REPLY_BAD_CMD), toId);
-        }
-      }
-      if (cmd === 'jerbonics') {
-        if (subCmd === 'add') {
-          utils.saveJerrism(event.body.split('/jerbonics add')[1].trim());
-        } else {
-          chat.sendMessage(utils.getJerryReply(), toId);
-        }
-      }
-      if (cmd === 'fanduel') {
-        const baseUrl = utils.getFanDuelBaseUrl(event);
-        const opts = {
-          uri: subCmd === 'info' ? baseUrl : `${baseUrl}/entries?page=1&page_size=10`,
-          headers: config.fanDuel.authHeader,
-          json: true,
-        };
-
-        if (subCmd === 'info') {
-          rp(opts).then((json) => {
-            const contest = json.contests[0];
-            const entered = contest.entries.count;
-            const startDate = contest.start_date;
-            const msg = `${contest.name}\u000A--\u000AID: ${contest.id}\u000AEntered: ${entered}/${contest.size.min}\u000AStarts In: ${moment().tz('America/New_York').preciseDiff(moment(startDate).tz('America/New_York'))}`;
-            chat.sendMessage(msg, toId);
-          }).catch(err => console.error(`fanduel info req failed: ${err}`));
-        }
-        if (utils.hasWords(subCmd, 'leaderboard', 'score', 'scores')) {
-          rp(opts).then(json =>
-            chat.sendMessage(utils.getFanDuelLeaderboard(json), toId)
-          ).catch(err => console.error(`[${cmd}] failed: ${err}`));
-        }
-      }
-      if (cmd === 'countdown') {
-        const endDate = config.cooldown.endDate[subCmd];
-        const diff = moment().preciseDiff(moment(endDate));
-        chat.sendMessage(`${_.lowerCase(subCmd)} (${moment.formatPref(endDate)})\u000A${diff}`, toId);
-      }
-      if (cmd === 'weather') {
-        const opts = {
-          uri: 'http://api.openweathermap.org/data/2.5/weather',
-          qs: {
-            units: 'imperial',
-            zip: `${subCmd || '97818'},us`,
-            appid: config.weather.API_KEY,
-          },
-          json: true,
-        };
-        rp(opts).then(json => chat.sendMessage(utils.getWeather(json), toId));
-      }
-      if (cmd === 'shrug') {
-        chat.sendMessage('¯\u005C_(ツ)_/¯', toId);
-      }
-      if (cmd === 'gif') {
-        const attachment = fs.createReadStream(`${DIR_GIF}/${subCmd}.gif`);
-        attachment.once('error', () => chat.sendMessage(utils.getRandomFromArray(REPLY_BAD_CMD), toId));
-        attachment.once('readable', () => chat.sendMessage({ attachment }, toId));
-      }
-      if (cmd === 'yoda') {
-        if (subCmd) {
-          const opts = _.assign(config.yoda.api, { qs: { sentence: subCmd } });
-          rp(opts).then(res => chat.sendMessage(res, toId));
-        } else {
-          chat.sendMessage(utils.getRandomFromArray(REPLY_BAD_CMD), toId);
-        }
-      }
-      if (cmd === 'joke') {
-        rp('http://api.yomomma.info').then(res => chat.sendMessage(JSON.parse(res).joke, toId));
-      }
-      if (cmd === '8' && event.body.match(/\w\?/)) {
-        rp(config['8ball'].api)
-        .then(res => chat.sendMessage(res, toId));
-      }
+    } else if (utils.isEmoji(event)) {
+      const msg = event.body === '🔥' ? '🔥' : emoji.random();
+      chat.sendMsg(msg, toId);
+    } else if (event.cmd) {
+      cmd(chat, event);
     } else if (utils.hasWords(event, 'LGH')) {
-      chat.sendMessage([{ body: '🔥' },
+      chat.sendMsg([{ body: '🔥' },
         { attachment: fs.createReadStream(`${DIR_GIF}/tank.gif`) }][_.random(1)],
         toId
       );
-    } else if (event.body) {
+    } else {
       const autoResponses = utils.getAutoResponses(event);
       utils.debug(autoResponses);
       if (!_.isEmpty(autoResponses)) {
-        sendMsg(utils.getRandomFromArray(autoResponses), toId);
+        chat.sendMsg(utils.getRandomFromArray(autoResponses), toId);
       }
     }
   });
@@ -264,4 +113,4 @@ require('facebook-chat-api')(credentials, (loginErr, chat) => {
     stopListening();
     console.log('shutdown: bot logged out');
   });
-});
+}).catch(err => console.error(`failed to login: ${err}`));
